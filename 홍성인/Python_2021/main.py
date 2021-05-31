90126756a35a9b7b4038f9b125a0f0bcecff7496
HongSungIn <donquixote827@gmail.com>
2021-05-31 18:18:39 +0900

    add0531
    add0531

--- 홍성인/Python_2021/main.py
+++ 홍성인/Python_2021/main.py
@@ -1,6 +1,33 @@
+from flask import Flask,render_template,request
+import cv2
+
 from ex01 import Ex01
 
-ex01 = Ex01()
-ex01.doSave()
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-print(ex01.doLoad())
+'''
+    templates 폴더에 html 파일들을 넣고
+    static 폴더에 image파일이나 js,css 등을 넣는다
+'''
+
+@app.route("/upload",methods=['POST'])
+def upload():
+    image = request.files['file']
+    image.save(f"static/{image.filename}")
+    print(image.filename)
+
+    npimage = cv2.imread(f"static/{image.filename}")
+    #npimage[0:100,0:100]=[0,0,0]
+    roi = npimage[0:100,100:200]
+    npimage[0:100,0:100] = roi
+    npimage[:,:,2]=0
+
+    npimagename = f"static/np{image.filename}"
+    cv2.imwrite(npimagename,npimage) #numpy 배열에 이미지화 저장
+
+    return render_template("print.html",image=image.filename,npimage=npimagename)
+
+app.run(host="127.0.0.1",port=5000);
